7eca416238d516a7bfb01fd50f655b8b8d8d9061
Rivo <107114580+Riv0J@users.noreply.github.com>
2023-09-20 23:07:25 +0200

    20/09

--- rivo/js/portfolio.js
+++ rivo/js/portfolio.js
@@ -1,5 +1,6 @@
-let RESPONSIVE_WIDTH = 768;
+const RESPONSIVE_WIDTH = 768;
 
+let header_initial_justify = 'center';
 let header_status = 'initial';
 let header_element = document.getElementById('nav');
 let personal_element = document.getElementById('personal');
@@ -7,28 +8,32 @@ let menu = document.getElementById('menu');
 
 //responsive variables
 let responsive_menu_button = document.getElementById("responsive_menu_button");
-let pc_menu_items_array = Array.from(menu.children).filter(item => item.id !== "responsive_menu_button");
+let desktop_menu_items_array = null;
 
 let responsive_mode = false;
 
-function isNewSizeResponsive() {
-    return window.innerWidth <= RESPONSIVE_WIDTH;
-}
 function initPortfolio(){
-    responsive_mode = isNewSizeResponsive();
+    responsive_mode = isWindowSizeResponsive();
     header_element = document.getElementById('nav'); 
     personal_element = document.getElementById('personal');
     menu = document.getElementById('menu');
     
     responsive_menu_button = document.getElementById("responsive_menu_button");
-    pc_menu_items_array = Array.from(menu.children).filter(item => item.id !== "responsive_menu_button");
+    desktop_menu_items_array = Array.from(menu.children).filter(item => item.id !== "responsive_menu_button");
 
     const desplegables = document.getElementsByClassName('desplegable');
+    addDropdownListeners(desplegables);
+
     if(responsive_mode == false){
-        layoutPC(desplegables, 'apply');
+        header_initial_justify = 'center';
+        layoutPC('apply');
+        layoutResponsive('remove');
     } else if(responsive_mode == true){
-        layoutResponsive();
+        header_initial_justify = 'flex-end';
+        layoutResponsive('apply');
+        layoutPC('remove');
     }
+    console.log(responsive_mode);
     window.addEventListener('scroll', () => {
         const posicionScroll = window.scrollY; 
         let new_status;
@@ -44,23 +49,23 @@ function initPortfolio(){
         adjustHeader(new_status);
     }); 
     window.addEventListener('resize', () => {
-        const window_resize_responsive = isNewSizeResponsive();
+        const window_resize_responsive = isWindowSizeResponsive();
         if(window_resize_responsive == responsive_mode){
             return;
         }
         if(responsive_mode == false){
-            layoutPC(desplegables,'apply');
+            header_initial_justify = 'center';
+            layoutPC('apply');
+            layoutResponsive('remove');
         } else {
+            header_initial_justify = 'flex-end';
             layoutResponsive('apply');
+            layoutPC('remove');
         }
         responsive_mode = window_resize_responsive;
         console.log("responsive change, new mode: "+responsive_mode);
-        layoutResponsive()
-      });
+    });
     populateIconClasses();
-}
-function rotateElement(element, degrees){
-    element.style.rotate = degrees+'deg';
 }
 function toggleDisplay(elements_array, new_display){
     let counter = 1;
@@ -88,81 +93,104 @@ function toggleDisplay(elements_array, new_display){
 function adjustHeader(new_state) {
     switch (new_state) {
         case 'initial':
+            header_element.style.justifyContent = header_initial_justify;
             personal_element.style.display = 'none';
             menu.style.opacity = 0;
             window.setTimeout(function() {
                 personal_element.style.opacity = 0;
                 menu.style.opacity = 1;
-            }, 50)
-            header_element.style.justifyContent = 'center';
+            }, 50);
             break;
         case 'scrolled':
+            header_element.style.justifyContent = 'space-between';
             personal_element.style.display = 'flex';
             menu.style.opacity = 0;
             window.setTimeout(function() {
                 personal_element.style.opacity = 1;
                 menu.style.opacity = 1;
-            }, 50)
-            header_element.style.justifyContent = 'space-between';
+            }, 50);
             break;
         default:
             console.log('default');
             break;
     }
+    
 }
-function layoutPC(desplegables, action){
-    switch (key) {
+function layoutPC(action){
+    switch (action) {
         case 'remove':
-            
+            toggleDesktopMenuItems('none')
             break;
         case 'apply':
-            toggleResponsiveMenu('none');
-            for (let i = 0; i < desplegables.length; i++) {
-                const desplegable = desplegables[i];
-                console.log(desplegable);
-        
-                const desplegable_icon = desplegable.querySelector("i");
-                console.log(desplegable_icon);
-        
-                const titles = desplegable.getElementsByClassName('deplegable_titulo');
-                const title = titles[0];
-                console.log(title);
-        
-                const options_containers = desplegable.getElementsByClassName('desplegable_options');
-                const options_container = options_containers[0];
-                const options = desplegable.getElementsByClassName('desplegable_option');
-                toggleDisplay(options,'none');
-        
-                desplegable.addEventListener('mouseenter', () => {
-                    if(options_container){
-                        options_container.style.display = 'flex';
-                        toggleDisplay(options,'block');
-                        if(desplegable_icon){
-                            rotateElement(desplegable_icon,'180')
-                        }
-                    }
-                });
-                desplegable.addEventListener('mouseleave', () => {
-                    if(options_container){
-                        options_container.style.display = 'none';
-                        toggleDisplay(options,'none');
-                        if(desplegable_icon){
-                            rotateElement(desplegable_icon,'0')
-                        }
-                    }   
-                });
-            }
+            toggleDesktopMenuItems('flex')
             break;
         default:
             break;
     }
 }
-function layoutResponsive(){
-    toggleResponsiveMenu('flex')
+function layoutResponsive(action){
+    switch (action) {
+        case 'remove':
+            toggleResponsiveMenu('none');
+            break;
+        case 'apply':
+            toggleResponsiveMenu('flex');
+            console.log('a');
+            header_element.style.justifyContent = header_initial_justify;
+            break;
+        default:
+            break;
+    }
 }
 function toggleResponsiveMenu(new_display){
-    responsive_menu_button.style.display == new_display;
+    responsive_menu_button.style.display = new_display;
+}
+function toggleDesktopMenuItems(new_display){
+    desktop_menu_items_array.forEach(item => {
+        item.style.display = new_display;
+    });
+}
+function isWindowSizeResponsive() {
+    return window.innerWidth <= RESPONSIVE_WIDTH;
+}
+function addDropdownListeners(desplegables){
+    for (let i = 0; i < desplegables.length; i++) {
+        const desplegable = desplegables[i];
+        console.log(desplegable);
+
+        const desplegable_icon = desplegable.querySelector("i");
+        console.log(desplegable_icon);
+
+        const titles = desplegable.getElementsByClassName('deplegable_titulo');
+        const title = titles[0];
+        console.log(title);
+
+        const options_containers = desplegable.getElementsByClassName('desplegable_options');
+        const options_container = options_containers[0];
+        const options = desplegable.getElementsByClassName('desplegable_option');
+        toggleDisplay(options,'none');
+
+        desplegable.addEventListener('mouseenter', () => {
+            if(options_container){
+                options_container.style.display = 'flex';
+                toggleDisplay(options,'block');
+                if(desplegable_icon){
+                    rotateElement(desplegable_icon,'180')
+                }
+            }
+        });
+        desplegable.addEventListener('mouseleave', () => {
+            if(options_container){
+                options_container.style.display = 'none';
+                toggleDisplay(options,'none');
+                if(desplegable_icon){
+                    rotateElement(desplegable_icon,'0')
+                }
+            }   
+        });
+    }
 }
 
-
-
+function rotateElement(element, degrees){
+    element.style.rotate = degrees+'deg';
+}
