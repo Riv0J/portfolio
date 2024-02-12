9bac08060dbf93d5d428bfd6433f4b599d10565f
Rivo <107114580+Riv0J@users.noreply.github.com>
2024-02-12 12:30:35 +0100

    ordenar

--- view/login.php
+++
@@ -1,8 +0,0 @@
-<h3> Login </h3>
-<form action="<?php echo $_SERVER['PHP_SELF']. '?section=login'; ?>" method='POST' class="login_form">
-    <label for="nombre">Nombre</label>
-    <input type="text" name="nombre">
-    <label for="pass">Contraseña</label> <!-- Cambiado de "nombre" a "pass" -->
-    <input type="password" name="pass">
-    <button type="submit">Iniciar sesión</button>
-</form>
--- view/panel_admin.php
+++
@@ -1,8 +0,0 @@
-<form action = 'index.php?section=admin' method='POST' id='panel_admin'>
-    <button class='icon_title' type='submit' name='action' value='reset_animales'>
-        <i data_icon_name='refresh'></i>Reactivar Animales
-    </button>
-    <button class='icon_title' type='submit' name='action' value='nuevo_animal_section'>
-        <i data_icon_name='add'></i>Nuevo Animal
-    </button>
-</form>
